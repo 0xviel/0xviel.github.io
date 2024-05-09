b770e9aceab757e5cd23234354b8dfdee63c0436
0xviel <ds2318014@gmail.com>
2024-05-09 18:15:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header>
+      <div class="nav-bar">
+        <a href="" class="logo">Logo</a>
+        <div class="navigation">
+          <div class="nav-items">
+            <i class="nav"></i>
+            <a href="#" class="uil"></a>
+            <a href="#" class="uil"></a>
+            <a href="#" class="uil"></a>
+            <a href="#" class="uil"></a>
+            <a href="#" class="uil"></a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <script src=""></script>
+  </body>
+</html>
